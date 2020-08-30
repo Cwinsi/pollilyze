b15ebd87c0bedcd610df9c3915d88a93edb4fd77
Cwinsi <grigortsevandrei@gmail.com>
2020-08-30 13:53:14 +0300

    init client app

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+const App: FunctionComponent = () => {
+  return (
+    <Switch>
+      <Route path='/' />
+    </Switch>
+  )
+}
+
+export default App
---
+++ client/webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                strictMath: true,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [require('tailwindcss'), require('autoprefixer')],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.webpack.js', '.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 8080,
+    historyApiFallback: true,
+  },
+}
